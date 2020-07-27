7b0896182c75045ef3a00ca865c6be996e86e511
dengchengH <42051766+dengchengH@users.noreply.github.com>
2020-07-27 12:36:22 +0800

    linklist

---
+++ LinkdedList/isCircle/isCircle.go
@@ -0,0 +1,21 @@
+package isCircle
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func HasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	fast := head.Next       // 快指针，每次走两步
+	for fast != nil && head != nil && fast.Next != nil {
+		if fast == head {   // 快慢指针相遇，表示有环
+			return true
+		}
+		fast = fast.Next.Next
+		head = head.Next        // 慢指针，每次走一步
+	}
+	return false
+}
